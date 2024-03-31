4ece69c4a15dfd70cb06803b21872dc9bf1c1fc3
KRSProg <meepo0nly2@gmail.com>
2024-03-31 10:36:28 +0100

    spring initlizr

---
+++ setbuilder/src/main/java/rupert/bear/setbuilder/SetbuilderApplication.java
@@ -0,0 +1,13 @@
+package rupert.bear.setbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetbuilderApplication.class, args);
+	}
+
+}
---
+++ setbuilder/src/test/java/rupert/bear/setbuilder/SetbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package rupert.bear.setbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
